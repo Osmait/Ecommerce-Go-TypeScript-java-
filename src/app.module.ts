e36a97a24c8c2013e4d55fe9d10ca07ccfcec3d2
osmait <saulburgos6@gmail.com>
2024-02-03 16:20:38 -0400

    add orderItem

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
 import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { OrderItemModule } from './order-item/order-item.module';
 
 @Module({
-  imports: [CategoryModule, UserModule, ProductModule, OrderModule, AuthModule],
+  imports: [CategoryModule, UserModule, ProductModule, OrderModule, AuthModule, OrderItemModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
