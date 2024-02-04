166dcc4041d4b04dc34c9a636488a9a9306190d8
osmait <saulburgos6@gmail.com>
2024-02-04 14:16:12 -0400

    add schema and validation at orderitem

--- src/order-item/services/orderItem.service.ts
+++ src/order-item/services/orderItem.service.ts
@@ -1,21 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { OrderItem } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { v4 as uuidv4 } from 'uuid';
+import { OrderItemRequestDto } from '../schema/orderItemRequest';
 @Injectable()
 export class OrderItemService {
   constructor(private prisma: PrismaService) {}
 
-  public async createOrderItem(orderRequest: OrderItem) {
-    const order = {
+  public async createOrderItem(orderRequest: OrderItemRequestDto) {
+    const product = await this.prisma.product.findFirst({
+      where: { id: orderRequest.productId },
+    });
+    if (!product) {
+      throw new BadRequestException();
+    }
+    const order = await this.prisma.order.findFirst({
+      where: { id: orderRequest.orderId },
+    });
+    if (!order || order.status != 'Active') {
+      throw new BadRequestException();
+    }
+    const orderItem = {
       id: uuidv4(),
-      productId: orderRequest.productId,
+      productId: product.id,
       quantity: orderRequest.quantity,
-      orderId: orderRequest.orderId,
+      orderId: order.id,
     };
-    await this.prisma.orderItem.create({ data: order });
+    await this.prisma.orderItem.create({ data: orderItem });
   }
   public async updateOrderItem(orderRequest: OrderItem, id: string) {
+    const product = await this.prisma.product.findFirst({
+      where: { id: orderRequest.productId },
+    });
+    if (!product) {
+      throw new BadRequestException();
+    }
+    const order = await this.prisma.order.findFirst({
+      where: { id: orderRequest.orderId },
+    });
+    if (!order || order.status != 'Active') {
+      throw new BadRequestException();
+    }
     await this.prisma.orderItem.update({ where: { id }, data: orderRequest });
   }
   public async deleteOrderItem(id: string) {
