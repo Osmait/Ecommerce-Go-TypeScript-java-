dcfab41b946b9f16e71bf8b2902fdb910b69ab01
osmait <saulburgos6@gmail.com>
2024-02-04 14:15:53 -0400

    add schema on controller

--- src/order-item/Controller/orderItem.controller.ts
+++ src/order-item/Controller/orderItem.controller.ts
@@ -3,12 +3,18 @@ import { OrderItemService } from '../services/orderItem.service';
 import { OrderItem } from '@prisma/client';
 import { ZodPipe } from 'src/zodpipe/zodpipe.pipe';
 import { idValidate, idValidateDto } from 'src/share/idValidate';
+import {
+  OrderItemRequest,
+  OrderItemRequestDto,
+} from '../schema/orderItemRequest';
 
 @Controller('order')
 export class OrderItemController {
   constructor(private orderItemServices: OrderItemService) {}
   @Post()
-  public createOrderItem(@Body() orderRequest: OrderItem) {
+  public createOrderItem(
+    @Body(new ZodPipe(OrderItemRequest)) orderRequest: OrderItemRequestDto,
+  ) {
     this.orderItemServices.createOrderItem(orderRequest);
   }
   @Put()
